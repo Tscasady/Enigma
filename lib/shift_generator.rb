55523f74da576ae8649e17286ba33bb258015c4a
Thomas Casady <tscasady@gmail.com>
2022-11-11 12:32:04 -0600

    feat: Add ShiftGenerator and initialize method.

--- lib/shift_generator.rb
+++ lib/shift_generator.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+# This class generates shifts for encrypting.
 class ShiftGenerator
-  
+  attr_reader :key, :date
+
+  def initialize(key, date)
+    @key = key
+    @date = date
+  end
 end
