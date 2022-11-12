d24c62db14066aa66dc246c6b0b36763567c4d67
Thomas Casady <tscasady@gmail.com>
2022-11-12 17:01:50 -0600

    test: Add encrypt_message test to enigma.

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,27 +1,37 @@
 # frozen_string_literal: true
 
 require './lib/enigma'
+require './lib/shift_generator'
 
 RSpec.describe Enigma do
   describe '#initialize' do
-    it 'exists and has readable attributes' do
+    it 'exists and has a character_set' do
+      enigma = Enigma.new
       expect(enigma).to be_a Enigma
-      expect(enigma.key).to eq ''
-      expect(enigma.date).to eq ''
+      expect(enigma.character_set.length).to eq 27
     end
   end
 
   describe '#encrypt' do
     it 'returns a hash containing an encrypted message, key, and date' do
-      expect(enigma.encrypt('encrypt test', '12345', '010122')).to eq({
-                                                                        encryption: '',
-                                                                        key: '',
-                                                                        date: 'd' })
-      expect(enigma.encrypt('a different test', '67899', '020222')).to eq({
-                                                                            encryption: '',
-                                                                            key: '',
-                                                                            date: 'd'
-                                                                          })
+      enigma = Enigma.new
+      expect(enigma.encrypt('test', '12345', '010122')).to eq({
+                                                                encryption: 'iigo',
+                                                                key: '12345',
+                                                                date: '010122'
+                                                              })
+      expect(enigma.encrypt('hello world', '02715', '040895')).to eq({
+                                                                       encryption: 'keder ohulw',
+                                                                       key: '02715',
+                                                                       date: '040895'
+                                                                     })
+    end
+  end
+
+  describe '#encrypt_message' do
+    it 'encrypts a message' do
+      shifts = ShiftGenerator.new('12345', '010122').shifts
+      expect(enigma.encrypt_message('test', shifts)).to eq 'iigo'
     end
   end
 end
