fa7cc2267bc00e635211a560a30ede79d0f683ae
Thomas Casady <tscasady@gmail.com>
2022-11-12 21:25:01 -0600

    feat: Map input arguments to variables and add puts statement.

---
+++ lib/encrypt.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require './lib/enigma'
+
+input = ARGV[0]
+output = ARGV[1]
+key = ARGV[2]
+date = ARGV[3]
+
+
+
+puts "Created #{output} with the key #{key} and date #{date}"
