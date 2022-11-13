9e9bb2a05dbbd0d0549afe608e2ce2faa609c960
Thomas Casady <tscasady@gmail.com>
2022-11-12 21:00:20 -0600

    feat: Add #decrypt_message helper method to enigma.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -13,11 +13,26 @@ class Enigma
     { encryption: encrypt_message(message, shifts), key: key, date: date }
   end
 
+  def decrypt(message, key, date = Time.now)
+    shifts = ShiftGenerator.new(key, date).shifts
+    { decryption: decrypt_message(message, shifts), key: key, date: date }
+  end
+
   def encrypt_message(message, shifts)
     shift_count = 0
     message.chars.map do |character|
       current_character = @character_set.find_index(character)
       new_character = (current_character + shifts[shift_count % 4]) % 27
+      shift_count += 1
+      @character_set[new_character]
+    end.join
+  end
+
+  def decrypt_message(message, shifts)
+    shift_count = 0
+    message.chars.map do |character|
+      current_character = @character_set.find_index(character)
+      new_character = (current_character - shifts[shift_count % 4]) % 27
       shift_count += 1
       @character_set[new_character]
     end.join
