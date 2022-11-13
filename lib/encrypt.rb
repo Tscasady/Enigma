875dce55b9848d9a631922d0269772b12af0f60a
Thomas Casady <tscasady@gmail.com>
2022-11-12 21:40:16 -0600

    fix: Change .read to .open, add nil checks to ARGV var assignments.

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,11 +2,12 @@
 
 require './lib/enigma'
 
+puts ARGV
 input = File.open("./data/#{ARGV[0]}", 'r')
-output = ARGV[1]
-key = ARGV[2]
-date = ARGV[3]
+output = File.open("./data/#{ARGV[1]}", 'w')
+key = ARGV[2] if !(ARGV[2].nil?)
+date = ARGV[3] if !(ARGV[3].nil?)
 
 enigma = Enigma.new
-enigma.encrypt(File.read(input), key, date)
+enigma.encrypt(File.read(input))
 puts "Created #{output} with the key #{key} and date #{date}"
