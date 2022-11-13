a14cd4a3fc88a44787478777dd923d39b6a388d3
Thomas Casady <tscasady@gmail.com>
2022-11-12 21:32:08 -0600

    feat: Add file.open command to ARGV inputs.

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,11 +2,11 @@
 
 require './lib/enigma'
 
-input = ARGV[0]
+input = File.open("./data/#{ARGV[0]}", 'r')
 output = ARGV[1]
 key = ARGV[2]
 date = ARGV[3]
 
-
-
+enigma = Enigma.new
+enigma.encrypt(File.read(input), key, date)
 puts "Created #{output} with the key #{key} and date #{date}"
