c5bcbf7c5eb96b2aa78f52f3e09d01e48a38619a
Thomas Casady <tscasady@gmail.com>
2022-11-13 20:48:36 -0600

    fix: Remove key/date args.

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,11 +2,8 @@
 
 require './lib/enigma'
 
-puts ARGV
 input = File.open("./data/#{ARGV[0]}", 'r')
 output = File.open("./data/#{ARGV[1]}", 'w')
-key = ARGV[2] if !(ARGV[2].nil?)
-date = ARGV[3] if !(ARGV[3].nil?)
 
 enigma = Enigma.new
 encrypted = enigma.encrypt(File.read(input))
