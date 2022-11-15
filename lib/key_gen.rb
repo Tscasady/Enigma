29a40413656d25420462d1782a2d91e23aa6368a
Thomas Casady <tscasady@gmail.com>
2022-11-15 13:50:15 -0600

    feat: Add key_gen class.

---
+++ lib/key_gen.rb
@@ -0,0 +1,21 @@
+class KeyGen
+  attr_reader :key
+
+  def initialize(key = nil, date = nil)
+    @key = key ||= generate_key
+  end
+
+  def generate_key
+    rand.to_s[2..6]
+  end
+
+  def find_key(message, shift, date)
+    d = date_to_offset(date).order
+    shift_order = @target_indicies.order
+    shift = []
+    message[-4..].chars.map.with_index do |character, index|
+      shift << @character_set.find_index(character) - shift_order[index]
+    end
+    shift.zip(d).map { |element| (element[0] - element[1]) % 27 } 
+  end
+end
