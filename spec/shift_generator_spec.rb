811261046dd564d39451ef4093376106c74c4613
Thomas Casady <tscasady@gmail.com>
2022-11-15 13:35:43 -0600

    test: Add new crack_shift test and remove relocated tests.

--- spec/shift_generator_spec.rb
+++ spec/shift_generator_spec.rb
@@ -18,30 +18,30 @@ RSpec.describe ShiftGenerator do
     end
   end
 
-  describe '#key_to_shifts' do
+  describe '#key_to_shift_part' do
     it 'transforms the key into 4 separate values for the shift' do
-      shift1 = ShiftGenerator.new('12345', '010122')
-      shift2 = ShiftGenerator.new('67899', '020222')
-      expect(shift1.key_to_shifts).to eq [12, 23, 34, 45]
-      expect(shift2.key_to_shifts).to eq [67, 78, 89, 99]
-    end
-  end
-
-  describe '#date_to_offset' do
-    it 'transforms a date string to an array of offsets' do
-      shift1 = ShiftGenerator.new('12345', '010122')
-      shift2 = ShiftGenerator.new('67899', '020222')
-      expect(shift1.date_to_offset).to eq([4, 8, 8, 4])
-      expect(shift2.date_to_offset).to eq([9, 2, 8, 4])
+      shift1 = ShiftGenerator.new('12345', '010122', 3)
+      shift2 = ShiftGenerator.new('67899', '020222', 2)
+      expect(shift1.key_to_shift_part).to eq [12, 23, 34, 45]
+      expect(shift2.key_to_shift_part).to eq [67, 78, 89, 99]
     end
   end
 
   describe '#shifts' do
     it 'adds key_shifts and offsets to produce final shifts' do
-      shift1 = ShiftGenerator.new('12345', '010122')
-      shift2 = ShiftGenerator.new('67899', '020222')
+      shift1 = ShiftGenerator.new('12345', '010122', 3)
+      shift2 = ShiftGenerator.new('67899', '020222', 2)
       expect(shift1.shifts).to eq([16, 31, 42, 49])
       expect(shift2.shifts).to eq([76, 80, 97, 103])
+    end
+  end
+
+  describe '#crack_shift' do
+    it 'rotates the target_indicies array by the given amount' do
+      shift1 = ShiftGenerator.new('12345', '010122', 3)
+      shift2 = ShiftGenerator.new('67899', '020222', 2)
+      expect(shift1.crack_shift).to eq [3, 27, 4, 13]
+      expect(shift2.crack_shift).to eq [13, 3, 27, 4]
     end
   end
 end
