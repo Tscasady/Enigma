514204786640ad459917c6d85a7809c4a59e7a78
Thomas Casady <tscasady@gmail.com>
2022-11-15 14:23:08 -0600

    test: Remove keys and offset attributes from SG.

--- spec/shift_generator_spec.rb
+++ spec/shift_generator_spec.rb
@@ -10,11 +10,7 @@ RSpec.describe ShiftGenerator do
       shift1 = ShiftGenerator.new(3, cipher)
       shift2 = ShiftGenerator.new(2, cipher)
       expect(shift1).to be_a ShiftGenerator
-      expect(shift1.key).to eq '12345'
-      expect(shift1.offset.date).to eq '010122'
       expect(shift1.shift_order).to eq 3
-      expect(shift2.key).to eq '67899'
-      expect(shift2.offset.date).to eq '020222'
       expect(shift2.shift_order).to eq 2
     end
   end
